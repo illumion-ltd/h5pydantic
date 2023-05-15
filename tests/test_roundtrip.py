b5c46d7ae87590ddac451b89d6a3b42ef6880a15
Clinton Roy <clintonr@ansto.gov.au>
2023-05-15 14:44:05 +1000

    TEST: the beginnings of some strategies to create dictionaries (which will be turned into pydantic models, to be dumped, then loaded, then we can test that the dump/load data matches

--- tests/test_roundtrip.py
+++ tests/test_roundtrip.py
@@ -13,3 +13,25 @@ def varname():
         st.text(head_alphabet, min_size=1, max_size=1),
         st.text(tail_alphabet))
 
+rectangle_lists = st.integers(min_value=0, max_value=10).flatmap(
+
+    lambda n: lists(lists(integers(), min_size=n, max_size=n))
+
+)
+
+# TODO add datasets to values, make it recursive.
+def values():
+    """A strategy that produced a valid HD5Group dictionary definition."""
+
+    def st_value(t):
+        
+
+    return st.one_of(st.integers(), st.floats()).flatmap(st_value)
+
+
+@given(st.dictionaries(min_size=1,
+                       keys=varname(),
+                       values=st.one_of(st.integers(), st.floats())))
+def test_roundtrip(d):
+    print(d)
+    assert False
