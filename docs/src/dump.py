6e77f2bacffd56b92275e80980d5b826cddb37d3
Clinton Roy <clintonr@ansto.gov.au>
2023-05-15 16:48:31 +1000

    DOC: update examples, still not perfect but can dump this at least.

--- docs/src/dump.py
+++ docs/src/dump.py
@@ -1,14 +1,16 @@
-from model import Experiment
+from model import Experiment, Acquisition
 
 import numpy
 from pathlib import Path
 
-shape = (1024, 1024)
+shape = (3, 5)
 
 experiment = Experiment(metadata={"start": {"temperature": 25.0, "humidity": 0.4},
                                   "end": {"temperature": 26.0, "humidity": 0.4}},
-                        data=[{"_data": numpy.random.randint(255, size=shape), "beamstop": 10},
-                              {"_data": numpy.random.randint(255, size=shape), "beamstop": 11},
-                              {"_data": numpy.random.randint(255, size=shape), "beamstop": 12}])
+                        data=[])
+
+experiment.data.append(Acquisition(data_=numpy.random.randint(255, size=shape), beamstop=10))
+experiment.data.append(Acquisition(data_=numpy.random.randint(255, size=shape), beamstop=11))
+experiment.data.append(Acquisition(data_=numpy.random.randint(255, size=shape), beamstop=12))
 
 experiment.dump(Path("experiment.hdf"))
