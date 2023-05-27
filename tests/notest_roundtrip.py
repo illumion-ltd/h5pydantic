d1ccd3d1f91c168372849b48eab8c296f81f3a68
Clinton Roy <clintonr@ansto.gov.au>
2023-05-27 20:44:32 +1000

    TEST: just enough hypothesis to get started.

--- tests/notest_roundtrip.py
+++ tests/notest_roundtrip.py
@@ -1,37 +1,51 @@
 from hypothesis import given, strategies as st
+from hypothesis.strategies import composite
+from pydantic import create_model
+
+from h5pydantic import H5Group
+
+import pytest
+
 import string
 
 
 def varname():
     """A strategy that produces a valid python variable name"""
 
-    head_alphabet = string.ascii_letters + "_"
-    tail_alphabet = head_alphabet + string.digits
+    head_alphabet = string.ascii_letters
+    tail_alphabet = head_alphabet + string.digits + "_"
 
     return st.builds(
         lambda a, b: a + b,
         st.text(head_alphabet, min_size=1, max_size=1),
         st.text(tail_alphabet))
 
-rectangle_lists = st.integers(min_value=0, max_value=10).flatmap(
-
-    lambda n: lists(lists(integers(), min_size=n, max_size=n))
-
-)
-
 # TODO add datasets to values, make it recursive.
-def values():
+# TODO handle NaN values
+# TODO calculcate the integers min/max properly
+@composite
+def value(draw, types=st.one_of(st.integers(min_value=-2**63, max_value=2**63), st.floats(allow_nan=False))):
     """A strategy that produced a valid HD5Group dictionary definition."""
-
-    def st_value(t):
-        
-
-    return st.one_of(st.integers(), st.floats()).flatmap(st_value)
+    val = draw(types)
+    return (type(val), val)
 
 
-@given(st.dictionaries(min_size=1,
-                       keys=varname(),
-                       values=st.one_of(st.integers(), st.floats())))
-def test_roundtrip(d):
-    print(d)
-    assert False
+
+@pytest.fixture(scope="session")
+def hdf_path(tmp_path_factory):
+    return tmp_path_factory.mktemp("data") / "roundtrip.hdf"
+
+@given(d=st.dictionaries(min_size=1,
+                         keys=varname(),
+                         values=value()))
+def test_roundtrip(d, hdf_path):
+    d["__base__"] = H5Group
+
+    dynamic_model = create_model("dynamic_model", **d)
+
+    output = dynamic_model()
+
+    output.dump(hdf_path)
+
+    with dynamic_model.load(hdf_path) as input:
+        assert output == input
