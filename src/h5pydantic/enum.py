1f248aaf8b9ff98e6c1768bd9ac4d45fefc97341
Clinton Roy <clintonr@ansto.gov.au>
2023-07-11 14:17:02 +1000

    Removed debug print.

--- src/h5pydantic/enum.py
+++ src/h5pydantic/enum.py
@@ -11,11 +11,11 @@ class H5Enum(int, Enum):
     @classmethod
     def _dump(cls, h5file: h5py.File, container, key: str, value: int, fieldtype: pydantic.fields.ModelField):
         # FIXME look for previous type? maybe cache it in the class?
-        h5type = h5py.h5t.enum_create(fieldtype.type_.h5dtype().h5pyid())
+        h5type = h5py.h5t.enum_create(fieldtype.type_.h5dtype().h5pyid)
         for (member_name, member_value) in fieldtype.type_.__members__.items():
             h5type.enum_insert(member_name.encode("ascii"), member_value)
 
-        container.attrs.create(key, dtype=h5type.dtype, data=value.value)
+        container.attrs.create(key, dtype=h5type.dtype, data=value)
 
     @classmethod
     def _load(cls, h5file: h5py.File, prefix: PurePosixPath, key: str, field: pydantic.fields.ModelField):
