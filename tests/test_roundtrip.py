c908f2937eade701ea0c4707c341e9977e5d9f25
Clinton Roy <clintonr@ansto.gov.au>
2023-07-16 22:46:21 +1000

    TEST: back to create_model, it does a bunch of stuff that new_class doesn't

--- tests/test_roundtrip.py
+++ tests/test_roundtrip.py
@@ -1,6 +1,7 @@
 from hypothesis import given, strategies as st
 from h5pydantic import H5Group, H5Dataset, H5DatasetConfig, H5Integer32, H5Integer64
-import numpy, string, types
+import numpy, string
+from pydantic import create_model
 
 def name_str(head_alphabet):
     return st.builds(str.__add__, st.sampled_from(head_alphabet), st.text(head_alphabet + string.digits + "_"))
@@ -12,14 +13,14 @@ def varname_st():
     return name_str(string.ascii_letters)
 
 @st.composite
-def type_and_value_st(draw, dataset: bool = True):
-    dtype = draw(st.sampled_from([H5Integer32, H5Integer64, float, str] + [H5Dataset, H5Group] * dataset))
-    return {H5Integer32: (lambda: (dtype, draw(st.integers(min_value=H5Integer32.ge, max_value=H5Integer32.le)))),
-            H5Integer64: (lambda: (dtype, draw(st.integers(min_value=H5Integer64.ge, max_value=H5Integer64.le)))),
-            float:       (lambda: (dtype, draw(st.floats(allow_nan=False)))),
-            str:         (lambda: (dtype, draw(st.text(string.printable)))),
-            H5Dataset:   (lambda: (draw(st.deferred(lambda: dataset_st())))),
-            H5Group:     (lambda: (draw(st.deferred(lambda: group_st())))),
+def type_and_value_st(draw, recursive:bool):
+    dtype = draw(st.sampled_from([H5Integer32, H5Integer64, float, str] + [H5Dataset, H5Group] * recursive))
+    return {H5Integer32: lambda: (dtype, draw(st.integers(min_value=H5Integer32.ge, max_value=H5Integer32.le))),
+            H5Integer64: lambda: (dtype, draw(st.integers(min_value=H5Integer64.ge, max_value=H5Integer64.le))),
+            float:       lambda: (dtype, draw(st.floats(allow_nan=False))),
+            str:         lambda: (dtype, draw(st.text(string.printable))),
+            H5Dataset:   lambda: (draw(dataset_st())),
+            H5Group:     lambda: (draw(group_st())),
             }[dtype]()
 
 @st.composite
@@ -28,7 +29,7 @@ def dataset_st(draw):
     ndims = draw(st.integers(min_value=1, max_value=3))
     shape = tuple(draw(st.integers(min_value=2, max_value=5)) for dim in range(ndims))
     d = draw(st.dictionaries(min_size=0, keys=varname_st(), values=type_and_value_st(False)))
-    dtype = types.new_class(classname, (H5Dataset,), kwds={"shape": shape, "dtype": H5Integer32})
+    dtype = create_model(classname, __base__=H5Dataset, __cls_kwargs__={"shape": shape, "dtype": H5Integer32}, **d)
     value = dtype()
     value.data(numpy.random.randint(-100, 100, size=shape))
     return (dtype, value)
@@ -36,8 +37,8 @@ def dataset_st(draw):
 @st.composite
 def group_st(draw):
     classname = draw(classname_st())
-    d = draw(st.dictionaries(min_size=1, keys=varname_st(), values=type_and_value_st()))
-    dtype = types.new_class(classname, (H5Group,), d)
+    d = draw(st.dictionaries(min_size=1, keys=varname_st(), values=type_and_value_st(True)))
+    dtype = create_model(classname, __base__=H5Group, **d)
     value = dtype()
     return (dtype, value)
 
