2c8454e57563340e202b3741e730e5e7e1e0397d
Clinton Roy <clintonr@ansto.gov.au>
2023-07-16 20:12:29 +1000

    Export datasetconfig, not sure it's really needed, but can't hurt.

--- src/h5pydantic/__init__.py
+++ src/h5pydantic/__init__.py
@@ -2,5 +2,5 @@ from importlib import metadata
 
 __version__ = metadata.version(__package__)
 
-from .model import H5Dataset, H5Group
+from .model import H5Dataset, H5DatasetConfig, H5Group
 from .types import H5Integer32, H5Integer64
