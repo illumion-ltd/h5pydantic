ef89fd410e736df634a99a07f4163b767647aff2
Clinton Roy <clintonr@ansto.gov.au>
2024-01-02 11:23:48 +1100

    ENH: get rid of the class completely, still need the dump/load though.

--- src/h5pydantic/__init__.py
+++ src/h5pydantic/__init__.py
@@ -2,6 +2,5 @@ from importlib import metadata
 
 __version__ = metadata.version(__package__)
 
-from .enum import H5IntEnum
 from .model import H5Dataset, H5DatasetConfig, H5Group
 from .types import H5Int32, H5Int64
--- src/h5pydantic/enum.py
+++ src/h5pydantic/enum.py
@@ -5,30 +5,15 @@ import h5py.h5t
 
 import pydantic.fields
 
-from enum import Enum, IntEnum
 
-# FIXME ideally, I'd get rid of this type completely.
-
-class H5Enum(int, Enum):
-    @classmethod
-    def _dump(cls, h5file: h5py.File, container, key: str, value: int, fieldtype: pydantic.fields.ModelField):
-        # FIXME look for previous type? maybe cache it in the class?
-        h5type = h5py.h5t.enum_create(fieldtype.type_.dtype().h5pyid)
-        for (member_name, member_value) in fieldtype.type_.__members__.items():
-            h5type.enum_insert(member_name.encode("ascii"), member_value)
+def _h5enum_dump(h5file: h5py.File, container, key: str, value: int, fieldtype: pydantic.fields.ModelField):
+    # FIXME look for previous type? maybe cache it in the class?
+    h5type = h5py.h5t.enum_create(fieldtype.type_.__h5dtype__.h5pyid)
+    for (member_name, member_value) in fieldtype.type_.__members__.items():
+        h5type.enum_insert(member_name.encode("ascii"), member_value)
 
         container.attrs.create(key, dtype=h5type.dtype, data=value)
 
-    @classmethod
-    def _load(cls, h5file: h5py.File, prefix: PurePosixPath, key: str, field: pydantic.fields.ModelField):
-        return field.type_(h5file[str(prefix)].attrs[key])
 
-
-class H5IntEnum(IntEnum):
-    """Wraps the HDF5 Integer Enum Type.
-
-    The dtype class keywoard argument is mandatory.
-    """
-    def __init_subclass__(self, **kwds):
-        # FIXME raise a nice alert if dtype kwarg is missing.
-        self.__class__.dtype = lambda x: kwds["dtype"]
+def _h5enum_load(h5file: h5py.File, prefix: PurePosixPath, key: str, field: pydantic.fields.ModelField):
+    return field.type_(h5file[str(prefix)].attrs[key])
