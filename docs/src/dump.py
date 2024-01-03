4047214450fbe88f97285721197b02b37623f2a0
Clinton Roy <clintonr@ansto.gov.au>
2024-01-03 11:08:32 +1100

    DOC: create all models before attempting to dump, so that they all get a reference to the underlying hdf5 file.

--- docs/src/dump.py
+++ docs/src/dump.py
@@ -1,17 +1,12 @@
 from model import Experiment, Acquisition, Baseline, Metadata
 
-import numpy
+import numpy as np
 from pathlib import Path
 
-experiment = Experiment(metadata=Metadata(start=Baseline(temperature=25.0, humidity=0.4),
+experiment = Experiment(data=[Acquisition(beamstop=11), Acquisition(beamstop=12)],
+                        metadata=Metadata(start=Baseline(temperature=25.0, humidity=0.4),
                                           end=Baseline(temperature=26.0, humidity=0.4)))
 
-acq = Acquisition(beamstop=11)
-acq.data(numpy.random.randint(255, size=Acquisition._h5config.shape))
-experiment.data.append(acq)
-
-acq = Acquisition(beamstop=12)
-acq.data(numpy.random.randint(255, size=Acquisition._h5config.shape))
-experiment.data.append(acq)
-
-experiment.dump(Path("experiment.hdf"))
+with experiment.dumper(Path("experiment.hdf")):
+    experiment.data[0][()] = np.random.randint(255, size=(3, 5))
+    experiment.data[1][()] = np.random.randint(255, size=(3, 5))
