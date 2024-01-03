4ca75e123d45e1594deda46437f071e9c524abef
Clinton Roy <clintonr@ansto.gov.au>
2024-01-03 11:09:07 +1100

    DOC: use the array getitem, instead of the old data_ attribute.

--- docs/src/load.py
+++ docs/src/load.py
@@ -2,5 +2,5 @@ from model import Experiment
 from pathlib import Path
 
 with Experiment.load(Path("experiment.hdf")) as experiment:
-    print(experiment.data[1].data_[:])
+    print(experiment.data[1][()])
     print(experiment.metadata.start.temperature)
