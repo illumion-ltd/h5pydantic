d98d39afc8e9053845bd8eee503f42ca3822d316
Clinton Roy <clintonr@ansto.gov.au>
2024-01-20 20:25:13 +1100

    ENH: partial dumping exception.

--- src/h5pydantic/__init__.py
+++ src/h5pydantic/__init__.py
@@ -2,5 +2,6 @@ from importlib import metadata
 
 __version__ = metadata.version(__package__)
 
+from .exceptions import H5PartialDump
 from .model import H5Dataset, H5DatasetConfig, H5Group
 from .types import H5Int32, H5Int64
